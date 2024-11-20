ddf56fcaf94a73c4a84234436a2a06cac1106a95
ddasni <danielsousamelo5@gmail.com>
2024-11-20 13:24:24 -0300

    modificações no cadastro

--- Cadastro.php
+++ Cadastro.php
@@ -18,26 +18,19 @@ if (isset($_POST['incluir'])) {
         && !empty($SenhaCadastro) && !empty($senhaConfirma))  {
 
         include "conexao.php";
-            // Verifica se senha e confirmar senha são identicos
-            if ($SenhaCadastro == $senhaConfirma) {
-                if ($user->cadastrar($NomeCadastro, $EnderecoCadastro, $EmailCadastro, $SenhaCadastro)) {
-                    ?>
-                    <script>
-                        alert("<?php echo $user->msgSucesso; ?>");
-                        // Aguardar o alerta ser fechado e depois redirecionar
-                        setTimeout(function() {
-                        window.location.href = "Login.html"; // Altere para a URL desejada
-                        }, 1000); // Aguarda 1 segundo antes de redirecionar
-                    </script>
-                    <?php
-                } 
-                else {
-                    echo "email já exisente - Erro: " . $user->msgErro;
-                }
-            } 
-            else {
-                echo "Senha e confirmar senha não conferen - Erro: " . $user->msgErro;
-            }
+        
+        // Verifica se senha e confirmar senha são identicos
+        if ($SenhaCadastro == $senhaConfirma) {
+
+            $user->cadastrar($NomeCadastro, $EnderecoCadastro, $EmailCadastro, $SenhaCadastro);
+            
+        } 
+        else {
+            echo "<script> alert('Senhas não conferem!') </script>";
+            $SenhaCadastro = null;
+            $senhaConfirma = null;
+            echo '<script> setTimeout(function() { window.location.href = "Cadastro.html"; }, 1000);</script>';
+        }
     }
     else { // se todos os campos não forem preenchidos
         echo "Preencha todos os campos - Erro: " . $user->msgErro;
