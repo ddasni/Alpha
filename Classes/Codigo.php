80ad33106198e148854dc4ed73eec2995545ecd0
ddasni <danielsousamelo5@gmail.com>
2024-11-26 11:08:27 -0300

    alterações no na verficação do status do sesssion

--- Classes/Codigo.php
+++ Classes/Codigo.php
@@ -2,32 +2,31 @@
 class Codigo {
 
     public function GerarCodigo() {
-        session_start();
 
-        // Gerar o código de 4 dígitos aleatorio
+        // verificação do status do session, se ela já foi chamada
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
         $codigo = str_pad(rand(0, 9999), 4, '0', STR_PAD_LEFT);
-
         $_SESSION['codigo'] = $codigo; 
     }
 
-    public function VerificarCodigo() 
-    {
-        session_start();
+    public function VerificarCodigo() {
+        
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
 
         $codigo_informado = $_SESSION['CodigoDigitado'];
 
-        // Verificando se o código informado é igual ao código armazenado na sessão
         if ($codigo_informado === $_SESSION['codigo']) {
-
             echo '<script> setTimeout(function() { window.location.href = "../2Login_Cadastro/TrocarSenha.php"; }, 1000);</script>';
-
-        } 
-        else {
+        } else {
             echo "<script> alert('Codigo incorreto. tente novamente!') </script>";
             echo '<script> setTimeout(function() { window.location.href = "../2Login_Cadastro/DigitarCodigo.php"; }, 1000);</script>';
         }
-
     }
-
 }
+
 ?>
