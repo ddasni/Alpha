f75c03276a1de7a45272f17036706746d8df88c8
ddasni <danielsousamelo5@gmail.com>
2024-11-29 16:59:04 -0300

    mudanças no arquivo tabela

--- Classes/Tabela.php
+++ Classes/Tabela.php
@@ -1,55 +1,50 @@
 <?php
 class Tabela {
     public function ChamarTabela(){
-        include_once "../Classes/Usuario.php";
-        $user = new Usuario;
-
-        include "conexao.php";
-
-        session_start();
-        $Email_User = $_SESSION['email_user'];
-
-        $user->sessao($Email_User);
+        include "../conexao.php";
 
         //Carrega a tabela
-        $Matriz=$conexao->prepare("SELECT TB_CLIENTE.NOME_CLIENTE, TB_CLIENTE.END_CLIENTE, 
-                                          TB_PEDIDO.DTA_PEDIDO, TB_PEDIDO.valor_total,
-                                   FROM clientes
-                                   INNER JOIN pedidos ON clientes.id_cliente = pedidos.id_cliente");
+        $Matriz=$conexao->prepare("SELECT TB_CLIENTE.NOME_CLIENTE, TB_CLIENTE.END_CLIENTE, TB_PEDIDO.ID_PEDIDO, TB_PEDIDO.DTA_PEDIDO, 
+                                          TB_PEDIDO.VALOR_PEDIDO, TB_PEDIDO.STATUS_PEDIDO, TB_PROD.NOME_PROD
+                                    FROM TB_CLIENTE
+                                    INNER JOIN TB_PEDIDO ON TB_CLIENTE.ID_CLIENTE = TB_PEDIDO.ID_CLIENTE
+                                    INNER JOIN TB_PROD ON TB_PEDIDO.ID_PROD = TB_PROD.ID_PROD");
         
         $Matriz->execute();
+
+        echo "<br>";
+        echo "<br>";
 
         echo "<table border=1>";
 
         echo "<tr>";
-        echo "<td> Número do Pedido </td>";
         echo "<td> Nome </td>";
+        echo "<td> Endereço </td>";
+        echo "<td> Número do Pedido </td>";
+        echo "<td> Nome do produto </td>";
         echo "<td> Data do Pedido </td>";
         echo "<td> Status do pedido </td>";
-        echo "<td> Condição do Pagamento </td>";
-        echo "<td> Forma de Pagamento </td>";
-        echo "<td> Valor Parcelado </td>";
+        echo "<td> Valor do Pedido </td>";
         echo "</tr>";
 
         while ($Linha = $Matriz->fetch(PDO::FETCH_OBJ)) 
         {
-
+            $NomeCliente = $Linha->NOME_CLIENTE;
+            $EndCliente = $Linha->END_CLIENTE;
             $idPedido = $Linha->ID_PEDIDO;
-            $Nome = $Linha->NOME_CONTATO;
+            $NomeProd = $Linha->NOME_PROD;
             $DTAPedido = $Linha->DTA_PEDIDO;
             $StatusPedido = $Linha->STATUS_PEDIDO;
-            $CondPagto = $Linha->COND_PAGTO;
-            $FormaPagto = $Linha->FORM_PAGTO;
-            $ValorParce = $Linha->VALOR_PARCE;
+            $ValorPedido = $Linha->VALOR_PEDIDO;
             
             echo "<tr>";
+            echo "<td>".$NomeCliente ." </td>";
+            echo "<td>".$EndCliente ."</td>";
             echo "<td>".$idPedido ." </td>";
-            echo "<td>".$Nome ." </td>";
+            echo "<td>".$NomeProd." </td>";
             echo "<td>".$DTAPedido ."</td>";
             echo "<td>".$StatusPedido ." </td>";
-            echo "<td>".$CondPagto ." </td>";
-            echo "<td>".$FormaPagto ."</td>";
-            echo "<td>".$ValorParce ." </td>";
+            echo "<td>".$ValorPedido ." </td>";
             echo "</tr>";
         }
 
